271659986a37e721475192f3ddce7ac176fa4715
mozart-martins <49461099+mozart-martins@users.noreply.github.com>
2021-09-21 13:47:33 -0300

    Add files via upload

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,12 +1,30 @@
+import axios from 'axios'
+
 const pokemonStyle = {
     "width": "18rem"
 }
 
+// Taking pokemon photo
+const getPokemonPhoto = async (address) => {
+    // front_default
+    if(address) {
+        const apiResult = await axios.get(address)
+        const photo = apiResult.data.sprites.front_default
+        return photo;
+    }
+}
+
 const Pokemon = props => {
+    const photo = getPokemonPhoto(props.address)
     return (
-        <div className="card bg-danger text-white" style={pokemonStyle}>
-            <div className="card-title">{props.name}</div>
-            <div className="card-text"> ??? </div>
+        <div className="col">
+            <div className="card bg-danger text-white my-3 justify-content-center" style={pokemonStyle}>
+                <img src={getPokemonPhoto(props.address)} alt={props.name} />
+                <div className="card-body">
+                    <div className="card-title">{props.name}</div>
+                    <a href={props.address} className="card-link">Profile</a>
+                </div>
+            </div>
         </div>
     )
 }
