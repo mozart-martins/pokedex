df8c4b781cbd6d9bcc1229a64b930a2b79e1c4eb
mozart-martins <49461099+mozart-martins@users.noreply.github.com>
2021-09-24 14:21:15 -0300

    Add files via upload

--- src/Components/Context/Context.js
+++ src/Components/Context/Context.js
@@ -1,17 +1,17 @@
-import { useState, createContext } from "react";
-import axios from "axios";
+import { createContext, useState } from "react"
+import axios from 'axios'
 
 export const PokemonContext = createContext();
 
 export const PokemonProvider = (props) => {
   const [pokemonList, setPokemonList] = useState()
 
-  // Take first Pokemon page
   const getPokemonList = async () => {
-    const apiResult = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?limit=10&offset=10"
-    )
+    const apiResult = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+
     setPokemonList(apiResult.data)
+
+    // console.log(value)
   }
 
   return (
