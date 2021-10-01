22ca4dbe94c08245446944eb068c64988b6ef534
mozart-martins <49461099+mozart-martins@users.noreply.github.com>
2021-10-01 14:29:38 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { PokemonProvider } from './Components/Context/Context';
+import { PokemonProvider } from './Components/Context/Context'
+import PokemonItemDetail from './Components/Pokemon/PokemonItemDetail'
 import Navbar from './Components/Layout/Navbar'
 import Section from './Components/Layout/Section'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <PokemonProvider>
-      <Navbar />
-      <Section />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Section} />
+          <Route path="/detail/:id" component={PokemonItemDetail} />
+        </Switch>
+      </BrowserRouter>
     </PokemonProvider>
   );
 }
